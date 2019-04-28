2d6105221ee74772e7ac9e899ec59e9746b2c4e4
fengyachun <15210983612@163.com>
2019-04-29 05:12:26 +0800

    Update get_region_pointfrom_P_present.m

--- get_region_pointfrom_P_present.m
+++ get_region_pointfrom_P_present.m
@@ -1,6 +1,5 @@
 function region_point = get_region_pointfrom_P_present(P_present,region_grow_group_index,each_region_point_num,index_sub_region )
-%UNTITLED15 此处显示有关此函数的摘要
-%   取出region index_sub_region 对应的区域的点云
+
 if index_sub_region==1
         s = 0;      
         L = each_region_point_num(index_sub_region);
